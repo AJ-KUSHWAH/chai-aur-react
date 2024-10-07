f625092fef7dcca56d86bfd8e1c522e7970f6f70
AJ-KUSHWAH <kushwahaj2707@gmail.com>
2024-10-07 16:56:52 +0530

    add currency converter

---
+++ 04currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+const useCurrencyInfo = (currency) => {
+  const [currencyInfo, setCurrencyInfo] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setCurrencyInfo(res[currency]));
+    console.log(currencyInfo);
+  }, [currency]);
+
+  return currencyInfo;
+};
+
+export default useCurrencyInfo;
