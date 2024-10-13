d8b9f5ce385c6b772e2844d33fd28df169dd3f24
AJ-KUSHWAH <kushwahaj2707@gmail.com>
2024-10-13 17:50:31 +0530

    Toggle theme

---
+++ 07theme-toggle/src/contexts/theme.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  lightTheme: () => {},
+  darkTheme: () => {},
+});
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ThemeContext.Provider;
